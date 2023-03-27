6b95f13351d5146f1341ea1082ec810ea5e02658
hayahumairoh <humairohhaya0922@gmail.com>
2023-03-27 09:32:57 +0700

    v1.3 menambahkan sc pada halaman proses.php

---
+++ proses.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  $angka1 = $_POST['num1'];
+  $angka2 = $_POST['num2'];
+  $operator = $_POST['dropdown'];
+
+
+  if ($operator == '+') {
+    $hasil = $num1 + $num2;
+  } else if ($dropdown == '-') {
+    $hasil = $num1 - $num2;
+  } else if ($dropdown == '*') {
+    $hasil = $num1 * $num2;
+  } else if ($dropdown == '/') {
+    $hasil = $num1 / $num2;
+  }
+
+
+  echo "Hasil operasi " . $num1 . " " . $dropdown . " " . $angka2 . " adalah " . $hasil;
+}
+?>
